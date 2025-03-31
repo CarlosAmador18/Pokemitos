c162fbd1063b3eb0bcaa9a8cd81de7bf18d00fdb
Carlos Amador <carlosamagin79@gmail.com>
2025-03-31 15:45:30 +0200

    p

---
+++ main/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import generics
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}

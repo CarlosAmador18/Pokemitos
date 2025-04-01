6be1020ccd03a7e8eb0e1c4d7b601083ffb4aace
Carlos Amador <carlosamagin79@gmail.com>
2025-04-01 09:20:59 +0200

    p

--- FRONTEND/frontend/src/App.jsx
+++ FRONTEND/frontend/src/App.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
-import {BrowserRouter as Navigate,Route,Routes} from'react-router-dom';
-import Login from '.pages/Login'
+import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+function logout() {
+  localStorage.clear();
+  return <Login/>
+}
+
+function RegisterAndLogout() {
+  localStorage.clear();
+  return <Register/>
+}
 
 function App() {
-  <>
-
-    </>
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/logout" element={<logout/>}/>
+          <Route path="/registerAndLogout" element={<RegisterAndLogout/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App
--- FRONTEND/frontend/src/components/Form.jsx
+++ FRONTEND/frontend/src/components/Form.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {ACCESS_TOKEN,REFRESH_TOKEN} from'../constants';
+import api from '../api';
+import '../styles/Form.css';
 
-function Form() {
+function Form({route, method}) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
@@ -21,7 +23,8 @@ function Form() {
       if (method === 'login') {
         localStorage.setItem(ACCESS_TOKEN, response.data.access);
         localStorage.setItem(REFRESH_TOKEN, response.data.refresh);
-        navigate('/');  } else {
+        navigate('/');  } 
+        else {
         navigate('/login');
       }
     } catch (error) {
@@ -31,10 +34,10 @@ function Form() {
 
 return (
   <div classname = 'bg-gray-100 min-h-screen flex items-center justify-center' >
-    <form onSubmit={handleSubmit} className='flex items-center'>
+    <form onSubmit={handleSubmit} className='flex items-center flex-col h-dvh justify-center gap-5'>
       <h1 className=''>{name}</h1>
       <input 
-        className='text-gray-200 rounded p-3 w-fit mb-4'
+        className='text-gray-200 rounded p-3 w-fit mb-4 focus:border-amber-700 border-amber-400'
         value={username}
         type='text'
         onChange={(e) => setUsername(e.target.value)}
@@ -48,11 +51,21 @@ return (
           onChange={(e) => setPassword(e.target.value)}
           placeholder='Password'
           required
-          className='text-gray-200 rounded p-3 w-fit mb-4' 
-        />
+          className='text-gray-200 rounded p-3 w-fit mb-4 focus:border-amber-700 border-amber-400 focus:placeholder:text-fuchsia-600'
+          />
 
-        <button className='p-2 '></button>
+        <button className='p-2 border-amber-300 rounded cursor-pointer'>{name}</button>
+
+        {method === "login" ? (
+          <a className='' href='/register'>Go to Register</a>
+        ) : (
+          <a className='' href='/login'>Go to Login</a>
+        )
+      }
     </form>
     </div>
+
     
 )}
+
+export default Form;
